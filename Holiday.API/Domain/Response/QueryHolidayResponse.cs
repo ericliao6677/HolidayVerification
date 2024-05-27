b2762cae315780bb77002e36b349da216a0f1f25
ericliao <chixiang777@gmail.com>
2024-05-27 17:06:29 +0800

    update: QueryHolidayResponse的IsHoliday型別改成string

--- Holiday.API/Domain/Response/QueryHolidayResponse.cs
+++ Holiday.API/Domain/Response/QueryHolidayResponse.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Date { get; set; }
         public string? Name { get; set; }
-        public bool IsHoliday { get; set; }
+        public string IsHoliday { get; set; }
         public string HolidayCategory { get; set; }
         public string? Description { get; set; }
     }
